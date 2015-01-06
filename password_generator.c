4ce79a39754690564b38dc9d9a3ac9a3e5894723
Florian Klink <flokli@flokli.de>
2015-01-06 21:42:08 +0100

    add password generator

---
+++ password_generator.c
@@ -0,0 +1,24 @@
+// from https://wrgms.com/synologys-secret-telnet-password/
+
+#include <stdlib.h> 
+#include <time.h> 
+#include <stdio.h> 
+
+void main()
+{
+    struct timeval tvTime;
+    struct tm tmOutput;
+
+    gettimeofday(&tvTime, 0);
+    localtime_r(&(tvTime.tv_sec), &tmOutput);
+
+    tmOutput.tm_mon += 1;
+    printf("password for today is: %x%02d-%02x%02d\n\n",
+        tmOutput.tm_mon, tmOutput.tm_mon, tmOutput.tm_mday,
+        gcd(tmOutput.tm_mon, tmOutput.tm_mday));
+}
+
+int gcd(int a, int b)
+{
+    return (b?gcd(b,a%b):a);
+}
